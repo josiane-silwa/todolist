25a29a85beb81030eedbca717afab538351163c5
Josiane Silva <josiannesilwa@gmail.com>
2023-10-25 22:57:09 -0300

    Initial commit

---
+++ js/todolist.js
@@ -0,0 +1,55 @@
+//criando botao fechar e appendando a cada item da lista
+var nodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < nodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  nodelist[i].appendChild(span);
+}
+
+//click no botao fechar para esconder o item 
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+
+//adiciona um simbolo checked quando um item é clicado
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
+
+//cria novo item ao clicar no botao adicionar
+function newElement() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("input").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("Escreva algo!");
+  } else {
+    document.getElementById("lista").appendChild(li);
+  }
+  document.getElementById("input").value = "";
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+} 
